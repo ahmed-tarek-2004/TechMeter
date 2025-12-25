fc1cf729c201bb1bf31e41fc48a0c8ae4fae44f9
Ahmed Tarek <ahmedtarek7580@gmail.com>
2025-12-25 11:25:09 +0200

    Initial Commit

---
+++ TechMeter.API/Extensions/APIServiceCollectionExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace TechMeter.Extensions
+{
+    public static class APIServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSwagger(this IServiceCollection service)
+        {
+            service.AddSwaggerGen(option =>
+            {
+                option.SwaggerDoc("v1", new OpenApiInfo { Title = "Tech Meter", Version = "v1" });
+
+                // For XML Comments we will use it later
+                //var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                //var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                //option.IncludeXmlComments(xmlPath);
+
+                option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter a valid token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+
+                option.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+            });
+            return service;
+        }
+    }
+}
