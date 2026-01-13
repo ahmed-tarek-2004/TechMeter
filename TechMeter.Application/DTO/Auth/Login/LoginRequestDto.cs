22bef3bc379f9d76ac5c036d992b5c63eea907e6
Ahmed Tarek <ahmedtarek7580@gmail.com>
2026-01-13 22:56:58 +0200

    basic-auth-operation

---
+++ TechMeter.Application/DTO/Auth/Login/LoginRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechMeter.Application.DTO.Auth.Login
+{
+    public record LoginRequestDto(string email,string password,string otp="");
+}
---
+++ TechMeter.Application/DTO/Auth/Register/StudentRegisterResponseDto.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechMeter.Domain.Enums;
+
+namespace TechMeter.Application.DTO.Auth.Register
+{
+    public class StudentRegisterResponseDto
+    {
+        public string Id { get; set; }
+        public string UserName {  get; set; }
+        public string PhoneNumber { get; set; }
+        public string Role { get; set; }
+        public string Country { get; set; }
+        public string? ProfileUrl { get; set; }
+        public Gender Gender { get; set; }
+        public string EmailAddress { get; set; }
+        public string EducationLeveL {  get; set; }
+        public int? Age { get; set; }
+        public bool isEmailConfirmed { get; set; }
+        public string accessToken { get; set; }
+        public string refreshToken { get; set; }
+        
+    }
+}
