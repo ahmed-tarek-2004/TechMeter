c7b8b0a2c234723be9eca37c441a40438fdf70c5
Ahmed Tarek <ahmedtarek7580@gmail.com>
2026-02-21 16:36:55 +0200

    AddingBasicLessonServices

--- TechMeter.Infrastructure/Migrations/20260221131645_AddingLEssonUrlForLEssonModule.Designer.cs
+++ TechMeter.Infrastructure/Migrations/20260221131645_AddingLEssonUrlForLEssonModule.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TechMeter.Infrastructure.Persistence;
 
@@ -11,9 +12,11 @@ using TechMeter.Infrastructure.Persistence;
 namespace TechMeter.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260221131645_AddingLEssonUrlForLEssonModule")]
+    partial class AddingLEssonUrlForLEssonModule
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
