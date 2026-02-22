1b8f0340dd44ec8b0544d2f6477eb1f8de6e10ad
Ahmed Tarek <ahmedtarek7580@gmail.com>
2026-02-23 00:36:58 +0200

    ApplyingStash

--- TechMeter.Application/Interfaces/CourseService/ICourseService.cs
+++ TechMeter.Application/Interfaces/CourseService/ICourseService.cs
@@ -13,12 +13,8 @@ namespace TechMeter.Application.Interfaces.CourseService
         Task<Response<GetCourseResponse>> GetCourseByIdAsync(string Id);
         Task<Response<List<GetCourseResponse>>> GetAllCoursesAsync();
         Task<Response<AddCourseResponse>> AddCourseAsync(string providerId,AddCourseRequest request);
-<<<<<<< Updated upstream:TechMeter.Application/Interfaces/CourseService/ICourseService.cs
-        Task<Response<GetCourseResponse>> EditCourseAsync(string providerId,EditCourseRequest request);
-=======
         Task<Response<List<GetCourseResponse>>> GetProviderCoursesAsync(string providerId);
         Task<Response<GetCourseResponse>> EditCourseAsync(string providerId, string courseId, EditCourseRequest request);
->>>>>>> Stashed changes:TechMeter.Application/Interfaces/Course/ICourseService.cs
         Task<Response<string>> DeleteCourseByIdAsync(string responsiableId,string courseId);
 
 
