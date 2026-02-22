5d61cd2492b2d33f2a426eb3afcd4ae86fbfd764
Ahmed Tarek <ahmedtarek7580@gmail.com>
2026-02-23 00:30:33 +0200

    AddingProviderGetCoursesEndPoint

--- TechMeter.Application/Interfaces/CourseService/ICourseService.cs
+++ TechMeter.Application/Interfaces/CourseService/ICourseService.cs
@@ -11,9 +11,14 @@ namespace TechMeter.Application.Interfaces.CourseService
     public interface ICourseService
     {
         Task<Response<GetCourseResponse>> GetCourseByIdAsync(string Id);
-        Task<Response<List<GetCourseResponse>>> GetCoursesAsync();
+        Task<Response<List<GetCourseResponse>>> GetAllCoursesAsync();
         Task<Response<AddCourseResponse>> AddCourseAsync(string providerId,AddCourseRequest request);
+<<<<<<< Updated upstream:TechMeter.Application/Interfaces/CourseService/ICourseService.cs
         Task<Response<GetCourseResponse>> EditCourseAsync(string providerId,EditCourseRequest request);
+=======
+        Task<Response<List<GetCourseResponse>>> GetProviderCoursesAsync(string providerId);
+        Task<Response<GetCourseResponse>> EditCourseAsync(string providerId, string courseId, EditCourseRequest request);
+>>>>>>> Stashed changes:TechMeter.Application/Interfaces/Course/ICourseService.cs
         Task<Response<string>> DeleteCourseByIdAsync(string responsiableId,string courseId);
 
 
