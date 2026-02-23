9692165ab95bddfb00058b57986f96b8d0d0dbb1
Ahmed Tarek <ahmedtarek7580@gmail.com>
2026-02-24 01:03:38 +0200

    AddingBasicInterfce&Dto

---
+++ TechMeter.Application/DTO/Cart/CartItemResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechMeter.Application.DTO.Cart
+{
+    internal class CartItemResponse
+    {
+    }
+}
