44d5e992aaa869250d5db355f05337034cff47ec
Ahmed Tarek <ahmedtarek7580@gmail.com>
2026-02-25 13:09:24 +0200

    AddingOrderToDI

--- TechMeter.Application/Interfaces/Order/IOrderService.cs
+++ TechMeter.Application/Interfaces/Order/IOrderService.cs
@@ -10,11 +10,11 @@ namespace TechMeter.Application.Interfaces.Order
 {
     public interface IOrderService
     {
-        public Task<Response<OrderResponse>> CreateClientOrder(string ClientId);
+        public Task<Response<OrderResponse>> CreateStudentOrder(string StudentId);
         public Task<Response<OrderResponse>> GetOrderById(string UsserId, string OrderId);
-        public Task<Response<PaginatedList<OrderSummaryResponse>>> GetClientOrders(string ClientId, GetOrders getOrders);
-        public Task<Response<PaginatedList<OrderSummaryResponse>>> GetSellerOrders(string sellerId, GetOrders getOrders);
-        public Task<Response<List<OrderResponse>>> GetSellOrdersDetails(string sellerId, string orderId);
+        public Task<Response<PaginatedList<OrderSummaryResponse>>> GetStudentOrders(string StudentId, GetOrders getOrders);
+        public Task<Response<PaginatedList<OrderSummaryResponse>>> GetProviderOrders(string ProviderId, GetOrders getOrders);
+        public Task<Response<List<OrderResponse>>> GetSellOrdersDetails(string ProviderId, string orderId);
         public Task<Response<PaginatedList<OrderSummaryResponse>>> GetAdminOrders(GetOrders getOrders);
         public Task<Response<OrderResponse>> UpdatOrderStatus(UpdateOrderStatus updateOrderStatus);
         public Task<Response<OrderResponse>> CancelOrderStatus(string orderId);
