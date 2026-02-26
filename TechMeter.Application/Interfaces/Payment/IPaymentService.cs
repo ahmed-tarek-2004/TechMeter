d7da4d2832fa941874ee794ec69cebf23d943952
Ahmed Tarek <ahmedtarek7580@gmail.com>
2026-02-27 00:01:47 +0200

    AddingPaymentService

--- TechMeter.Application/Interfaces/Payment/IPaymentService.cs
+++ TechMeter.Application/Interfaces/Payment/IPaymentService.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TechMeter.Application.DTO.Payment;
+using TechMeter.Domain.Shared.Bases;
 
 namespace TechMeter.Application.Interfaces.Payment
 {
     public interface IPaymentService
     {
-        //Task<PaymentResponse> CreateACheckOut(PaymentRequest paymentRequest);
+        Task<Response<PaymentResponse>> CreateACheckOut(string UserId,PaymentRequest paymentRequest);
         //Task<PaymentResponse> WebHook(PaymentRequest paymentRequest);
     }
 }
