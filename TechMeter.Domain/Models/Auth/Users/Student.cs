8c444d04ea481346feb3e4615dd095b8c0e4ca6e
Ahmed Tarek <ahmedtarek7580@gmail.com>
2026-02-26 15:04:11 +0200

    AddingPaymentTransactionTables

--- TechMeter.Domain/Models/Auth/Users/Student.cs
+++ TechMeter.Domain/Models/Auth/Users/Student.cs
@@ -17,6 +17,8 @@ namespace TechMeter.Domain.Models.Auth.Users
         public Wishlist? Wishlist { get; set; }
         public ICollection<Order> Orders { get; set; } = new List<Order>(); 
         public ICollection<Course> Courses { get; set; } = new List<Course>();
+        public ICollection<PaymentTransaction> Transactions { get; set; } = new List<PaymentTransaction>();
+
         //public ICollection<CourseStudent> CourseStudent { get; set; } = new List<CourseStudent>(); 
     }
 }
