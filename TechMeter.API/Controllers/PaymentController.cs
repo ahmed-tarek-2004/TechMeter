b793e5dcba540f82376b72db80ef0e56de8e2680
Ahmed Tarek <ahmedtarek7580@gmail.com>
2026-02-28 00:52:21 +0200

    InitialPayment

--- TechMeter.API/Controllers/PaymentController.cs
+++ TechMeter.API/Controllers/PaymentController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using TechMeter.Application.DTO.Payment;
 using TechMeter.Application.Interfaces.Payment;
 using TechMeter.Domain.Shared.Bases;
 
@@ -15,6 +17,15 @@ namespace TechMeter.API.Controllers
         public PaymentController(IPaymentService paymentService)
         {
             _paymentService = paymentService;
+        }
+
+        [HttpPost("check-out")]
+        public async Task<ActionResult<PaymentResponse>> CheckoutAsync([FromBody] PaymentRequest request)
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var response = await _paymentService.CreateACheckOut(userId,request);
+
+            return StatusCode((int)response.StatusCode, response);
         }
     }
 }
--- TechMeter.Infrastructure/Services/Payment/PaymentService.cs
+++ TechMeter.Infrastructure/Services/Payment/PaymentService.cs
@@ -10,6 +10,7 @@ using TechMeter.Application.Interfaces.Payment;
 using TechMeter.Domain.Models.Auth.Identity;
 using TechMeter.Domain.Shared.Bases;
 using TechMeter.Infrastructure.Persistence;
+using static System.Net.WebRequestMethods;
 
 namespace TechMeter.Infrastructure.Services.Payment
 {
@@ -26,10 +27,10 @@ namespace TechMeter.Infrastructure.Services.Payment
         public async Task<Response<PaymentResponse>> CreateACheckOut(string studentId, PaymentRequest request)
         {
             var user = await _context.Users.FindAsync(studentId);
-            if (user == null)
-            {
-                return _responseHandler.BadRequest<PaymentResponse>("User is not found");
-            }
+            //if (user == null)
+            //{
+            //    return _responseHandler.BadRequest<PaymentResponse>("User is not found");
+            //}
             var options = new SessionCreateOptions
             {
                 PaymentMethodTypes = new List<string> { "card" },
@@ -40,22 +41,24 @@ namespace TechMeter.Infrastructure.Services.Payment
                     PriceData = new SessionLineItemPriceDataOptions
                     {
                         Currency = request.Currency,
+                        UnitAmountDecimal=request.Price,
                         ProductData = new SessionLineItemPriceDataProductDataOptions
                         {
-                            Name = request.CourseName
+                            Name = request.CourseName,
                         },
                     },
+                    Quantity=1
                 },
             },
                 Mode = "payment",
-                SuccessUrl = request.SuccessUrl,
-                CancelUrl = request.CancelUrl,
-                CustomerEmail = user.Email,
-                Metadata = new Dictionary<string, string>
-             {
-                { "orderId", request.OrderId },
-                { "clientId", user.Id }
-             }
+                SuccessUrl = "https://amars-marvelous-site-305200.webflow.io/",
+                CancelUrl = "https://amars-fantabulous-site-16cb2e.webflow.io/",
+                //CustomerEmail = user.Email,
+                //   Metadata = new Dictionary<string, string>
+                //{
+                //   { "orderId", request.OrderId },
+                //   { "clientId", user.Id }
+                //}
             };
 
             var service = new SessionService();
@@ -64,7 +67,7 @@ namespace TechMeter.Infrastructure.Services.Payment
             var response = new PaymentResponse()
             {
                 SessionId = session.Id,
-                SessionUrl = session.SuccessUrl
+                SessionUrl = session.Url,
             };
             return _responseHandler.Success(response, "Continue to pay");
         }
