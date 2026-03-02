9a039a18967f944f69b5a6e7c5054561c37dd80a
Ahmed Tarek <ahmedtarek7580@gmail.com>
2026-03-03 01:08:50 +0200

    AddingPaymentIntentController

--- TechMeter.API/Controllers/PaymentController.cs
+++ TechMeter.API/Controllers/PaymentController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 using System.Security.Claims;
+using TechMeter.API.Validators.Payment;
 using TechMeter.Application.DTO.Payment;
 using TechMeter.Application.Interfaces.Payment;
 using TechMeter.Domain.Shared.Bases;
@@ -14,10 +17,15 @@ namespace TechMeter.API.Controllers
     public class PaymentController : ControllerBase
     {
         private readonly IPaymentService _paymentService;
+        public readonly ResponseHandler _responseHandler;
+        private readonly IValidator<PaymentRequest> _paymentRequestValidator;
 
-        public PaymentController(IPaymentService paymentService)
+        public PaymentController(IPaymentService paymentService, IValidator<PaymentRequest> paymentRequestValidator,
+            ResponseHandler responseHandler)
         {
             _paymentService = paymentService;
+            _paymentRequestValidator = paymentRequestValidator;
+            _responseHandler = responseHandler;
         }
 
         [HttpPost("check-out")]
@@ -28,6 +36,22 @@ namespace TechMeter.API.Controllers
             var response = await _paymentService.CreateACheckOut(userId, request);
 
             return StatusCode((int)response.StatusCode, response);
+        }
+        [HttpPost("create-payment-intent")]
+        [Authorize(Roles = "student")]
+        public async Task<ActionResult<Response<PaymentIntentResponse>>> CreatePaymentIntent([FromBody] PaymentRequest request)
+        {
+            var Validation = await _paymentRequestValidator.ValidateAsync(request);
+            if (!Validation.IsValid)
+            {
+                var Error = string.Join(',', Validation.Errors.Select(b => b.ErrorMessage));
+                return StatusCode((int)HttpStatusCode.BadRequest, _responseHandler.BadRequest<PaymentResponse>(Error));
+            }
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var response = await _paymentService.PaymentIntentService(userId, request);
+
+            return StatusCode((int)response.StatusCode, response);
+
         }
         [HttpPost("webhook")]
         public async Task<ActionResult<object>> StripeWebhook()
---
+++ TechMeter.API/Validators/Payment/PaymentRequestValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using TechMeter.Application.DTO.Payment;
+
+namespace TechMeter.API.Validators.Payment
+{
+    public class PaymentRequestValidator : AbstractValidator<PaymentRequest>
+    {
+        public PaymentRequestValidator()
+        {
+            RuleFor(b => b)
+                    .Must(b => !string.IsNullOrEmpty(b.OrderId))
+                    .WithMessage("Order Id Is Required");
+
+            RuleFor(b => b)
+                    .Must(b => !string.IsNullOrEmpty(b.Currency))
+                    .WithMessage("Currency Is Required");
+        }
+    }
+}
