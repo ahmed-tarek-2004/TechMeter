36351262b862b00f1a76ce7042aeb160edc5576c
Ahmed Tarek <ahmedtarek7580@gmail.com>
2026-03-10 22:36:07 +0200

    ApplyingRatingService

---
+++ TechMeter.Application/DTO/Rating/AddStudentRatingRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechMeter.Application.DTO.Rating
+{
+    public class AddStudentRatingRequest
+    {
+        public string CourseId { get; set; }
+        public int Rating { get; set; } = 0;
+        public string? Comment { get; set; } = string.Empty;
+        public DateTime RatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ TechMeter.Application/DTO/Rating/EditStudentRatingRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechMeter.Application.DTO.Rating
+{
+    public class EditStudentRatingRequest
+    {
+        public string CourseId { get; set; }
+        public int? Rating { get; set; }
+        public string? Comment { get; set; }
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
