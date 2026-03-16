3597dba5768bc2fe918baf0f42822bcea052aed9
Ahmed Tarek <ahmedtarek7580@gmail.com>
2026-03-16 14:50:02 +0200

    AddingOtpStatusAndVrifyResetPasswordStatusResponse

---
+++ TechMeter.Application/DTO/Auth/ResetPassword/VerifyResetPasswordResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechMeter.Application.DTO.Auth.ResetPassword
+{
+    public class VerifyResetPasswordResponse
+    {
+        public string token { get; set; }
+    }
+}
