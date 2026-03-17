a78e810a2b64b493312f452787bd2ea40287ad3e
Ahmed Tarek <ahmedtarek7580@gmail.com>
2026-03-17 14:43:59 +0200

    refacotor ExceptionHandler to be more flexable

--- TechMeter.API/Common/Exceptions/GlobalExceptionHandler.cs
+++ TechMeter.API/Common/Exceptions/GlobalExceptionHandler.cs
@@ -8,9 +8,11 @@ namespace TechMeter.API.Common.Exceptions
     public class GlobalExceptionHandler : IExceptionHandler
     {
         private readonly ILogger<GlobalExceptionHandler> _logger;
-        public GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger)
+        private readonly IProblemDetailsService _problemDetailsService;
+        public GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger,IProblemDetailsService problemDetailsService)
         {
             _logger = logger;
+            _problemDetailsService = problemDetailsService;
         }
 
         public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
@@ -25,7 +27,12 @@ namespace TechMeter.API.Common.Exceptions
             };
             _logger.LogError(exception.Message);
             httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-            await httpContext.Response.WriteAsJsonAsync(problemDeatils, cancellationToken: default);
+            // await httpContext.Response.WriteAsJsonAsync(problemDeatils, cancellationToken: default);
+            await _problemDetailsService.WriteAsync(new ProblemDetailsContext
+            {
+                ProblemDetails = problemDeatils,
+                HttpContext = httpContext,
+            });
             return true;
         }
     }
