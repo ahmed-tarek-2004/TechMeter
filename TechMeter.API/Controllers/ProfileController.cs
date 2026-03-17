5868631c3640691fa7979f4cb3175fff2d5cfbae
Ahmed Tarek <ahmedtarek7580@gmail.com>
2026-03-18 01:29:02 +0200

    EndPoitnsForStudentProfile

--- TechMeter.API/Controllers/ProfileController.cs
+++ TechMeter.API/Controllers/ProfileController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
+using TechMeter.Application.DTO.Profile;
 using TechMeter.Application.DTO.User;
 using TechMeter.Application.Interfaces.UserProfile;
 using TechMeter.Domain.Shared.Bases;
@@ -15,7 +16,7 @@ namespace TechMeter.API.Controllers
 
         [Authorize(Roles = "provider")]
         [HttpGet("provider/profile")]
-        public async Task<ActionResult<Response<GetProviderProfileInfoResponse>>> GetproviderProfile()
+        public async Task<ActionResult<Response<GetProviderProfileInfoResponse>>> GetProviderProfileAsync()
         {
             var providerId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
             var response = await profileService.GetProviderInfoResponseAsync(providerId ?? "");
@@ -24,11 +25,29 @@ namespace TechMeter.API.Controllers
 
         [Authorize(Roles = "provider")]
         [HttpPut("provider/update/profile")]
-        public async Task<ActionResult<Response<EditProviderProfileRequest>>> UpdateproviderProfile([FromForm] EditProviderProfileRequest request)
+        public async Task<ActionResult<Response<EditProviderProfileRequest>>> UpdateProviderProfileAsync([FromForm] EditProviderProfileRequest request)
         {
 
             var providerId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
             var response = await profileService.EditProviderProfileAsync(providerId ?? "", request);
+            return StatusCode((int)response.StatusCode, response);
+        }
+        [Authorize(Roles = "student")]
+        [HttpGet("student/profile")]
+        public async Task<ActionResult<Response<GetProviderProfileInfoResponse>>> GetStudentProfileAsync()
+        {
+            var studentId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var response = await profileService.GetStudentInfoResponseAsync(studentId ?? "");
+            return StatusCode((int)response.StatusCode, response);
+        }
+
+        [Authorize(Roles = "student")]
+        [HttpPut("student/update/profile")]
+        public async Task<ActionResult<Response<EditProviderProfileRequest>>> UpdateStudentProfileAsync([FromForm] EditStudentProfileRequest request)
+        {
+
+            var studentId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var response = await profileService.EditStudentProfileAsync(studentId ?? "", request);
             return StatusCode((int)response.StatusCode, response);
         }
     }
