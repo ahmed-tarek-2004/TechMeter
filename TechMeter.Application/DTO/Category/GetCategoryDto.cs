4b720abd9cb2fdf0a6731f470b8b7971b96bc4e3
Ahmed Tarek <ahmedtarek7580@gmail.com>
2026-04-02 21:04:12 +0200

    Refacor Category

--- TechMeter.Application/DTO/Category/GetCategoryDto.cs
+++ TechMeter.Application/DTO/Category/GetCategoryDto.cs
@@ -12,6 +12,6 @@ namespace TechMeter.Application.DTO.Category
         public string Id { get; set; }
         public string Name { get; set; }
         public string? Description { get; set; }
-        public List<GetCourseResponse>?courses { get; set; }
+        public IEnumerable<GetCourseResponse>?courses { get; set; }
     }
 }
