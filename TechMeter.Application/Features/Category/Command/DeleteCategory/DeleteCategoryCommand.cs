38b91509e47ea3ce2caf014840375fee69ca31a3
Ahmed Tarek <ahmedtarek7580@gmail.com>
2026-04-03 18:38:41 +0200

    Delete Category to cqrs

---
+++ TechMeter.Application/Features/Category/Command/DeleteCategory/DeleteCategoryCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechMeter.Domain.Shared.Bases;
+
+namespace TechMeter.Application.Features.Category.Command.DeleteCategory
+{
+    public sealed record DeleteCategoryCommand(string categoryId) : IRequest<Response<string>>;
+}
---
+++ TechMeter.Application/Features/Category/Command/DeleteCategory/DeleteCategoryCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechMeter.Application.Interfaces.Category;
+using TechMeter.Domain.Shared.Bases;
+
+namespace TechMeter.Application.Features.Category.Command.DeleteCategory
+{
+    public class DeleteCategoryCommandHandler(ICategoryService categoryService) : IRequestHandler<DeleteCategoryCommand, Response<string>>
+    {
+        public async Task<Response<string>> Handle(DeleteCategoryCommand request, CancellationToken cancellationToken)
+        {
+            return await categoryService.DeleteCategoryByIdAsync(request.categoryId);
+        }
+    }
+}
