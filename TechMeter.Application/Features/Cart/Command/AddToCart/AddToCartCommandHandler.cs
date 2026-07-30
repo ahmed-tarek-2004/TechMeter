f3827a99ff7620e6f4258d07197011bf26827cf5
Ahmed Tarek <ahmedtarek7580@gmail.com>
2026-07-30 03:54:45 +0300

    refactor/cart/cart-handler

--- TechMeter.Application/Features/Cart/Command/AddToCart/AddToCartCommandHandler.cs
+++ TechMeter.Application/Features/Cart/Command/AddToCart/AddToCartCommandHandler.cs
@@ -1,19 +1,84 @@
 ﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TechMeter.Application.Common;
 using TechMeter.Application.Interfaces.Services.Cart;
+using TechMeter.Domain.Models;
 using TechMeter.Domain.Shared.Bases;
 
 namespace TechMeter.Application.Features.Cart.Command.AddToCart
 {
-    public class AddToCartCommandHandler(ICartService cartService) : IRequestHandler<AddToCartCommand, Response<string>>
+    public class AddToCartCommandHandler(IApplicationDbContext context,ResponseHandler responseHandler,
+        ILogger<AddToCartCommandHandler>logger) : IRequestHandler<AddToCartCommand, Response<string>>
     {
         public async Task<Response<string>> Handle(AddToCartCommand request, CancellationToken cancellationToken)
         {
-            return await cartService.AddToCartAsync(request.StudentId, request.CourseId);//, request.UnitPrice);
+            try
+            {
+                var Course = await context.Course.FirstOrDefaultAsync(p => p.Id == request.CourseId);
+                if (Course == null)
+                {
+                    return responseHandler.BadRequest<string>("Course is not found");
+                }
+
+
+                var cart = await context.Cart
+                    .Include(b => b.CartItems)
+                    //.ThenInclude(b => b.Course)
+                    .FirstOrDefaultAsync(c => c.StudentId == request.StudentId);
+
+                if (cart == null)
+                {
+                    cart = new()
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        StudentId = request.StudentId,
+                        CreatedAt = DateTime.Now,
+                        UpdatedAt = DateTime.Now,
+                    };
+                    await context.Cart.AddAsync(cart);
+                }
+                if (cart.CartItems == null)
+                {
+                    cart.CartItems = new List<CartItem>();
+                }
+                var existingCartItem = cart.CartItems.Any(b => b.CourseId == request.CourseId);
+                if (existingCartItem == false)
+                {
+                    var CartItem = new CartItem()
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        CourseId = request.CourseId,
+                        UnitPrice = Course.Price,
+                        CreatedAt = DateTime.Now,
+                        CartId = cart.Id,
+                    };
+                    cart.CartItems.Add(CartItem);
+                    //await context.AddAsync(CartItem);
+                }
+                else
+                {
+                    return responseHandler.BadRequest<string>($"course {request.CourseId} already in the cart for this student");
+                }
+
+                await context.SaveChangesAsync(cancellationToken);
+                //var cartResponse = CreateCartResponse(cart);
+                //await transaction.CommitAsync();
+                return responseHandler.Success(string.Empty, "Course Add To Cart Successfully");
+
+            }
+            catch (Exception ex)
+            {
+                //await transaction.RollbackAsync();
+                logger.LogError(ex.Message);
+                return responseHandler.InternalServerError<string>(ex.Message);
+            }
+
         }
     }
 }
--- TechMeter.Application/Features/Cart/Command/RemoveCartItem/RemoveCartItemCommandHandler.cs
+++ TechMeter.Application/Features/Cart/Command/RemoveCartItem/RemoveCartItemCommandHandler.cs
@@ -1,21 +1,63 @@
 ﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TechMeter.Application.Common;
 using TechMeter.Application.DTO.Cart;
+using TechMeter.Application.Features.Cart.Command.AddToCart;
 using TechMeter.Application.Interfaces.Services.Cart;
+using TechMeter.Domain.Models.Auth.Users;
 using TechMeter.Domain.Shared.Bases;
 
 namespace TechMeter.Application.Features.Cart.Command.RemoveCartItem
 {
-    public class RemoveCartItemCommandHandler(ICartService cartService) : IRequestHandler<RemoveCartItemCommand, Response<string>>
+    public class RemoveCartItemCommandHandler(IApplicationDbContext context, ResponseHandler responseHandler,
+        ILogger<AddToCartCommandHandler> logger) : IRequestHandler<RemoveCartItemCommand, Response<string>>
     {
         
         public async Task<Response<string>> Handle(RemoveCartItemCommand request, CancellationToken cancellationToken)
         {
-            return await cartService.RemoveFromCartAsync(request.studentId, request.cartItemId);
+            var user = await context.Student.FirstOrDefaultAsync(b => b.Id == request.studentId);
+            if (user == null)
+            {
+                logger.LogWarning("Student not found for StudentId: {StudentId}", request.studentId);
+                return responseHandler.NotFound<string>("Student not found.");
+            }
+            try
+            {
+                var cart = await context.Cart
+               .Include(x => x.CartItems.Where(b => b.Id == request.cartItemId))
+               // .ThenInclude(c => c.Course)
+               .FirstOrDefaultAsync(b => b.StudentId == request.studentId);
+                if (cart == null)
+                {
+                    logger.LogWarning("Cart not found for StudentId: {StudentId}", request.studentId);
+                    return responseHandler.NotFound<string>("no Cart has found for studnet.");
+                }
+                var cartItem = cart.CartItems.FirstOrDefault();
+                if (cartItem == null)
+                {
+                    logger.LogWarning("CartItem not found. Cart: {CartId}", cart.Id);
+                    return responseHandler.NotFound<string>($"no CartItem found. Cart: {cart.Id}");
+                }
+                context.CartItem.Remove(cartItem);
+                cart.UpdatedAt = DateTime.UtcNow;
+
+                await context.SaveChangesAsync(cancellationToken);
+
+                //var response = CreateCartResponse(cart);
+                logger.LogInformation("Cart item removed successfully for StudentId: {StudentId}", request.studentId);
+                return responseHandler.Deleted<string>("Cart item removed successfully.");
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Error occurred while removing cart item for StudentId: {StudentId}", request.studentId);
+                return responseHandler.InternalServerError<string>("An error occurred while removing cart item.");
+            }
         }
     }
 }
